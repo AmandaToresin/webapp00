4c23e35d34d4a4e24152e8886a875ce1dde47cf8
Amanda Toresin <toresinamanda23@gmail.com>
2024-10-24 13:46:35 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -2,13 +2,17 @@
 import streamlit as st
   
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("Bemarco Estruturas")
+st.title("Memorial de Calculo")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("Setor comercial")
+st.header("Descomplicando os Dados de Memorial de Cálculo na Construção")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("alo alo")
+st.subheader("O que é Memorial de Cálculo?")
 
 # Use st.write("") para adicionar um texto ao seu Web app
-st.write("Bom dia")
+st.write("O memorial de cálculo é um documento que detalha todos os cálculos necessários para o projeto. Ele serve como um guia para a execução e a verificação das estruturas, garantindo que tudo esteja dentro das normas e regulamentos.")
+
+st.subheader("Importância dos Dados")
+
+st.write("Os dados contidos no memorial de cálculo são cruciais para a confiabilidade do projeto. Eles ajudam a evitar erros, economizar recursos e garantir a sustentabilidade das construções. Sem eles, o risco de falhas aumenta significativamente.")
