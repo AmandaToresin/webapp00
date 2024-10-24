e38c1dfb9d8f5da487bd5e76c94be7ed88cb388b
AmandaToresin <toresinamanda23@gmail.com>
2024-10-23 21:57:09 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -5,10 +5,10 @@ import streamlit as st
 st.title("Oi Amô")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("Love U")
+st.header("te amo muito em mo")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("Sub Cabeçalho")
+st.subheader("Lindo")
 
 # Use st.write("") para adicionar um texto ao seu Web app
-st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
+st.write("Gostoso do Caraio")
