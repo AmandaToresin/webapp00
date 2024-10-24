b7e0d3cec3804e5651e298a0b93b31fe462a3ffc
Amanda Toresin <toresinamanda23@gmail.com>
2024-10-24 13:34:32 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -2,13 +2,13 @@
 import streamlit as st
   
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("Oi Amô")
+st.title("Bemarco Estruturas")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("te amo muito em mo")
+st.header("Setor comercial")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("Lindo")
+st.subheader("alo alo")
 
 # Use st.write("") para adicionar um texto ao seu Web app
-st.write("Gostoso do Caraio")
+st.write("Bom dia")
