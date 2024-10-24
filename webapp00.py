77638fb425bee09cfdd121d1066bfbc5e5231644
Amanda Toresin <toresinamanda23@gmail.com>
2024-10-24 13:49:41 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -17,4 +17,4 @@ st.subheader("Importância dos Dados")
 
 st.write("Os dados contidos no memorial de cálculo são cruciais para a confiabilidade do projeto. Eles ajudam a evitar erros, economizar recursos e garantir a sustentabilidade das construções. Sem eles, o risco de falhas aumenta significativamente.")
 
-st.image("https://www.google.com/url?sa=i&url=https%3A%2F%2Fconube.com.br%2Fblog%2Fmercado-de-trabalho-para-engenheiros-no-brasil%2F&psig=AOvVaw0gj1ru0HeHMxSpbkIuTLWt&ust=1729874877788000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCLD3vZ-8p4kDFQAAAAAdAAAAABAE", caption="Engenheiros", use_column_width=True)
+st.image("https://www.google.com/url?sa=i&url=https%3A%2F%2Fbr.pinterest.com%2Fpin%2F821062575826976807%2F&psig=AOvVaw2t18is2Oddeb_vKKA0u7_8&ust=1729874953357000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCJD2x8K8p4kDFQAAAAAdAAAAABAE", caption="Engenheiros", use_column_width=True)
