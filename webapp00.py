a84c230b2e900c571e52afbfd30451d505d23e5e
Amanda Toresin <toresinamanda23@gmail.com>
2024-11-30 19:32:32 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -10,6 +10,13 @@ st.write("Os dados contidos no memorial de cálculo são cruciais para a confiab
 
 st.image("https://images.tcdn.com.br/img/img_prod/614225/piso_ceramico_interno_polido_avolio_66x66cm_cx_2_18m_formigres_81143_2_b834d5c845372194ba34b12f1882dfb6.jpg", caption="Engenheiros", use_column_width=True)
 
+
+
+# Segunda opcao
+
+st.subheader("Piso Cerâmico Retificado 66x66cm Caixa com 2,18 m² Extra Marmi Delux Mate Formigres")
+st.image("https://telhanorte.vtexassets.com/arquivos/ids/1245914/7899436339296.jpg?v=638119047284200000", caption="Engenheiros", use_column_width=True)
+
 def calcular_orcamento(area, preco_por_caixa, adicionar_sobra):
     """
     Calcula o orçamento total para o piso com base na área, preço por caixa e a opção de adicionar 20% de sobra.
@@ -39,8 +46,3 @@ if st.button("Calcular Orçamento"):
     orcamento, caixas_necessarias = calcular_orcamento(area, preco_por_caixa, adicionar_sobra)
     st.success(f"### O orçamento total para o piso é: R$ {orcamento:.2f}")
     st.info(f"Você precisará de aproximadamente {caixas_necessarias:.0f} caixas de piso.")
-
-# Segunda opcao
-
-st.subheader("Piso Cerâmico Retificado 66x66cm Caixa com 2,18 m² Extra Marmi Delux Mate Formigres")
-st.image("https://telhanorte.vtexassets.com/arquivos/ids/1245914/7899436339296.jpg?v=638119047284200000", caption="Engenheiros", use_column_width=True)
